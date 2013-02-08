1bf5fb62fa5ec1aaa6f028d29a41ee4d8c26e920
Jack <jack.wgm@gmail.com>
2013-02-08 13:05:36 +0800

    remove thread test on main.cpp
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- src/avhttp.cpp
+++ src/avhttp.cpp
@@ -11,13 +11,6 @@
 
 #include "avhttp.hpp"
 
-void test(avhttp::http_stream &h)
-{
-	boost::this_thread::sleep(boost::posix_time::seconds(5));
-	boost::system::error_code ec;
-	h.close(ec);
-}
-
 int main(int argc, char* argv[])
 {
 	boost::asio::io_service io;
@@ -27,8 +20,6 @@ int main(int argc, char* argv[])
 	opt.insert("Connection", "close");
 	h.request_options(opt);
 	h.open("http://w.qq.com/cgi-bin/get_group_pic?pic={64A234EE-8657-DA63-B7F4-C7718460F461}.gif");
-
-	boost::thread(boost::bind(&test, boost::ref(h)));
 
 	boost::array<char, 1024> buf;
 	boost::system::error_code ec;
