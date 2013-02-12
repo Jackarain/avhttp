481189c30f099795d7f0a1705b845c488cb4d067
Jack <jack.wgm@gmail.com>
2013-02-12 23:33:27 +0800

    fixed newline warning
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- include/avhttp/detail/bind_protect.hpp
+++ include/avhttp/detail/bind_protect.hpp
@@ -47,8 +47,7 @@ public:
 	}
 
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1> result_type operator()(const A1 & a1)
 	{
@@ -72,8 +71,7 @@ public:
 		return f_(a1, a2);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2> result_type operator()(A1 const & a1, A2 & a2)
 	{
@@ -117,8 +115,7 @@ public:
 		return f_(a1, a2, a3);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3)
 	{
@@ -142,8 +139,7 @@ public:
 		return f_(a1, a2, a3, a4);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4)
 	{
@@ -167,8 +163,7 @@ public:
 		return f_(a1, a2, a3, a4, a5);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4, class A5> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4, A5 const & a5)
 	{
@@ -192,8 +187,7 @@ public:
 		return f_(a1, a2, a3, a4, a5, a6);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4, class A5, class A6> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4, A5 const & a5, A6 const & a6)
 	{
@@ -217,8 +211,7 @@ public:
 		return f_(a1, a2, a3, a4, a5, a6, a7);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4, class A5, class A6, class A7> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4, A5 const & a5, A6 const & a6, A7 const & a7)
 	{
@@ -242,8 +235,7 @@ public:
 		return f_(a1, a2, a3, a4, a5, a6, a7, a8);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4, class A5, class A6, class A7, class A8> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4, A5 const & a5, A6 const & a6, A7 const & a7, A8 const & a8)
 	{
@@ -267,8 +259,7 @@ public:
 		return f_(a1, a2, a3, a4, a5, a6, a7, a8, a9);
 	}
 
-#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) \
-	&& !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
+#if !defined(BOOST_NO_FUNCTION_TEMPLATE_ORDERING) && !BOOST_WORKAROUND(__EDG_VERSION__, <= 238)
 
 	template<class A1, class A2, class A3, class A4, class A5, class A6, class A7, class A8, class A9> result_type operator()(A1 const & a1, A2 const & a2, A3 const & a3, A4 const & a4, A5 const & a5, A6 const & a6, A7 const & a7, A8 const & a8, A9 const & a9)
 	{
