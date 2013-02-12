09c4d406bf9b91427280368009fa015dd720b5e9
Jack <jack.wgm@gmail.com>
2013-02-12 23:34:50 +0800

    remove disable wanring on msvc
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- include/avhttp/detail/abi_prefix.hpp
+++ include/avhttp/detail/abi_prefix.hpp
@@ -12,19 +12,6 @@
 
 #include <boost/config/abi_prefix.hpp>
 
-// Disable some pesky MSVC warnings.
-#if defined(_MSC_VER)
-# pragma warning (push)
-# pragma warning (disable:4127)
-# pragma warning (disable:4251)
-# pragma warning (disable:4355)
-# pragma warning (disable:4512)
-# pragma warning (disable:4996)
-# pragma warning (disable:4267)
-# pragma warning (disable:4819)
-# pragma warning (disable:4244)
-#endif // defined(_MSC_VER)
-
 // Force external visibility of all types.
 #if defined(__GNUC__)
 # if (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)
--- include/avhttp/detail/abi_suffix.hpp
+++ include/avhttp/detail/abi_suffix.hpp
@@ -10,10 +10,6 @@
 
 // No include guard.
 
-#if defined(_MSC_VER)
-# pragma warning (pop)
-#endif // defined(_MSC_VER)
-
 #if defined(__GNUC__)
 # if (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) || (__GNUC__ > 4)
 #  pragma GCC visibility pop
