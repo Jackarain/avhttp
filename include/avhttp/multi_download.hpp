01a96f4fbebdcc20d75fe695a48509af6d036e09
Jack <jack.wgm@gmail.com>
2013-02-17 01:01:34 +0800

    add multi_download.hpp
    Signed-off-by: Jack <jack.wgm@gmail.com>

---
+++ include/avhttp/multi_download.hpp
@@ -0,0 +1,57 @@
+//
+// multi_download.hpp
+// ~~~~~~~~~~~~~~~~~~
+//
+// Copyright (c) 2013 Jack (jack dot wgm at gmail dot com)
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// path LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef MULTI_DOWNLOAD_HPP__
+#define MULTI_DOWNLOAD_HPP__
+
+#pragma once
+
+#include <boost/noncopyable.hpp>
+
+#include "http_stream.hpp"
+
+/************************************************************************
+boost::asio::io_service io;
+
+multi_download mget(io);
+mget.open(url); // async_open(url);
+mget.close();
+
+struct handle_reader
+{
+	boost::uint64_t offset;
+	std::size_t size;
+	std::vector<char> data;
+};
+
+boost::system::error_code ec;
+handle_reader hr;
+mget.read_some(hr, ec);
+
+mget.async_read_some(hr, handler);
+
+void handler(boost::system::error_code ec)
+{
+}
+
+************************************************************************/
+
+class multi_download : public boost::noncopyable
+{
+public:
+	multi_download() {}
+	~multi_download() {}
+
+public:
+
+private:
+};
+
+#endif // MULTI_DOWNLOAD_HPP__
