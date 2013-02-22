748e5a0025188a624e26319ecff62faa70b4c143
Jack <jack.wgm@gmail.com>
2013-02-23 02:04:15 +0800

    update example multi_download.cpp
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- example/multi_download.cpp
+++ example/multi_download.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <boost/array.hpp>
-#include <boost/progress.hpp>
 #include <cmath>
 
 #include "avhttp.hpp"
@@ -60,7 +59,6 @@ std::string add_suffix(float val, char const* suffix = 0)
 	return ret;
 }
 
-
 int main(int argc, char* argv[])
 {
 	if (argc != 2)
@@ -68,12 +66,15 @@ int main(int argc, char* argv[])
 		std::cerr << "usage: " << argv[0] << " <url>\n";
 		return -1;
 	}
-
 	try {
 		boost::asio::io_service io;
 		avhttp::multi_download d(io);
 
-		d.start(argv[1]);
+		avhttp::settings s;
+		// s.m_download_rate_limit = 102400;
+
+		d.start(argv[1], s);
+
 		if (d.file_size() != -1)
 			std::cout << "file \'" << d.file_name().c_str() <<
 			"\' size is: " << "(" << d.file_size() << " bytes) " << add_suffix(d.file_size()).c_str() << std::endl;
@@ -82,19 +83,34 @@ int main(int argc, char* argv[])
 
 		if (d.file_size() != -1)
 		{
-			boost::progress_display show_progress(d.file_size());
-			while (show_progress.count() != d.file_size())
+			printf("\n");
+			int percent = 0;
+			boost::int64_t file_size = d.file_size();
+			boost::int64_t bytes_download = 0;
+			while (percent != 100)
 			{
+				bytes_download = d.bytes_download();
+				percent = ((double)bytes_download / (double)file_size) * 100.0f;
 				boost::this_thread::sleep(boost::posix_time::millisec(200));
-				show_progress += (d.bytes_download() - show_progress.count());
+				printf("\r");
+				printf("%3d%% [", percent);
+				int progress = percent / 2;
+				for (int i = 0; i < progress; i++)
+					printf("=");
+				if (progress != 50)
+					printf(">");
+				for (int i = 0; i < 49 - progress; i++)
+					printf(" ");
+				printf("]  %s  %s/s", add_suffix(bytes_download).c_str(), add_suffix(d.download_rate()).c_str());
 			}
-
-			std::cout << "\n*** download completed! ***\n";
+			printf("\n");
 		}
 
 		t.join();
+
+		std::cout << "\n*** download completed! ***\n";
 	}
-	catch (boost::system::system_error &e)
+	catch (std::exception &e)
 	{
 		std::cerr << e.what() << std::endl;
 		return -1;
