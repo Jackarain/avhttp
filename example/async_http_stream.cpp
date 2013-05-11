eddebb5b1bf7b14a5e0d3f9f6f558d24edbfcdd4
Jack <jack.wgm@gmail.com>
2013-05-11 18:20:27 +0800

    改善example的实现.
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- example/async_http_stream.cpp
+++ example/async_http_stream.cpp
@@ -1,40 +1,58 @@
 #include <iostream>
+#include <fstream>
+
+#include <boost/filesystem.hpp>
 #include <boost/array.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
 #include "avhttp.hpp"
 
-class downloader
+class downloader : public boost::enable_shared_from_this<downloader>
 {
 public:
-	downloader(boost::asio::io_service &io, const std::string &url)
+	downloader(boost::asio::io_service &io)
 		: m_io_service(io)
 		, m_stream(io)
-	{
-		// avhttp::request_opts opt;
-		// opt.insert("Range", "bytes=0-2");
-		// m_stream.request_options(opt);
-
-		// https://2.gravatar.com/avatar/767fc9c115a1b989744c755db47feb60
-		// http://www.boost.org/LICENSE_1_0.txt
-		// http://w.qq.com/cgi-bin/get_group_pic?pic={64A234EE-8657-DA63-B7F4-C7718460F461}.gif
-
-		m_stream.check_certificate(false);
-		m_stream.async_open(url,
-			boost::bind(&downloader::handle_open, this, boost::asio::placeholders::error));
-	}
+	{}
 	~downloader()
 	{}
 
 public:
+	void start(const std::string &url)
+	{
+		avhttp::proxy_settings p;
+		p.type = avhttp::proxy_settings::http;
+		p.hostname = "218.188.13.237";
+		p.port = 8888;
+
+		// 设置代理服务器.
+		// m_stream.proxy(p);
+
+		// 设置处理https不进行认证.
+		m_stream.check_certificate(false);
+
+		avhttp::request_opts opt;
+		opt.insert(avhttp::http_options::accept_encoding, "gzip");
+		// 设置接受gzip格式, 需要启用AVHTTP_ENABLE_ZLIB.
+		// m_stream.request_options(opt);
+
+		m_stream.async_open(url,
+			boost::bind(&downloader::handle_open, shared_from_this(), boost::asio::placeholders::error));
+	}
+
 	void handle_open(const boost::system::error_code &ec)
 	{
 		if (!ec)
 		{
-			boost::asio::async_read(m_stream, boost::asio::buffer(m_buffer, 597),
-				boost::bind(&downloader::handle_read, this,
-				boost::asio::placeholders::bytes_transferred,
-				boost::asio::placeholders::error));
-// 			也可以: m_stream.async_read_some(boost::asio::buffer(m_buffer),
-// 				boost::bind(&downloader::handle_read, this,
+			m_stream.async_read_some(boost::asio::buffer(m_buffer, 1024),
+					boost::bind(&downloader::handle_read, shared_from_this(),
+					boost::asio::placeholders::bytes_transferred,
+					boost::asio::placeholders::error
+				)
+			);
+//			也可以:
+// 			boost::asio::async_read(m_stream, boost::asio::buffer(m_buffer, 597),
+// 				boost::bind(&downloader::handle_read, shared_from_this(),
 // 				boost::asio::placeholders::bytes_transferred,
 // 				boost::asio::placeholders::error));
 		}
@@ -46,14 +64,17 @@ public:
 		{
 			std::cout.write(m_buffer.data(), bytes_transferred);
 			m_stream.async_read_some(boost::asio::buffer(m_buffer),
-				boost::bind(&downloader::handle_read, this,
-				boost::asio::placeholders::bytes_transferred,
-				boost::asio::placeholders::error));
+					boost::bind(&downloader::handle_read, shared_from_this(),
+					boost::asio::placeholders::bytes_transferred,
+					boost::asio::placeholders::error
+				)
+			);
 		}
-		if (ec == boost::asio::error::eof)
+		else if (ec == boost::asio::error::eof)
 		{
 			std::cout.write(m_buffer.data(), bytes_transferred);
 		}
+		std::cout.flush();
 	}
 
 private:
@@ -71,8 +92,39 @@ int main(int argc, char* argv[])
 	}
 
 	boost::asio::io_service io;
+	std::fstream file;
+	std::string filename = argv[1];
+	bool urllist = false;
 
-	downloader d(io, argv[1]);
+	// 如果是存在的文件, 则查看是否是txt文件, 允许.txt文件中多个url.
+	if (boost::filesystem::exists(filename))
+	{
+		if (boost::filesystem::extension(filename) == ".txt")
+		{
+			urllist = true;
+			file.open(filename);
+		}
+	}
+
+	for (;;)
+	{
+		if (!urllist)	// 单个文件下载.
+		{
+			boost::shared_ptr<downloader> d(new downloader(io));
+			d->start(filename);
+			break;
+		}
+		else			// 从url列表中下载, txt文件中的每一行是一个url.
+		{
+			std::string url;
+			std::getline(file, url);
+			if (url.empty())
+				break;
+			boost::shared_ptr<downloader> d(new downloader(io));
+			d->start(url);
+		}
+	}
+
 	io.run();
 
 	return 0;
