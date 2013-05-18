30535dd3f5b14beec60ae216ab07a8af86dc823f
Jack <jack.wgm@gmail.com>
2013-05-18 22:42:17 +0800

    添加日志模块说明.
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- include/avhttp/logging.hpp
+++ include/avhttp/logging.hpp
@@ -29,6 +29,24 @@ namespace avhttp {
 namespace fs = boost::filesystem;
 
 ///内部使用的简易日志类.
+// 使用说明:
+//	在程序入口(如:main)函数调用 INIT_LOGGER 宏, 它有两个参数, 第一个参数指定了日志文件保存
+//	的路径, 第二个参数指定了日志文件保存的文件名, 详细见INIT_LOGGER.
+//	然后就可以使用LOG_DEBUG/LOG_INFO/LOG_WARNING/LOG_ERROR这几个宏来输出日志信息.
+// @begin example
+//  #include "avhttp.hpp" // avhttp.hpp 已经包含 logging.hpp, 也可单独包含logging.hpp.
+//  int main()
+//  {
+//     INIT_LOGGER(".", "example.log");	// 在当前目录创建日志文件为example.log.
+//     // 也可 INIT_LOGGER("", ""); 空串为参数来禁止输出日志到文件, 仅输出到控制台.
+//     LOG_DEBUG("Initialized.");
+//     std::string result = do_something();
+//     LOG_DEBUG("do_something return : " << result);	// 输出do_something返回结果到日志.
+//     ...
+//     UNINIT_LOGGER();	// 卸载日志模块.
+//  }
+// @end example
+
 class logger : public boost::noncopyable
 {
 public:
@@ -155,11 +173,16 @@ Logger_ptr& logger_single(std::string path = ".",
 //////////////////////////////////////////////////////////////////////////
 // 日志相关外部接口定义.
 
+///初始化日志接口.
+// @param path指定了日志文件保存的路径.
+// @param file指定了日志文件名.
+// 备注: 如果file为空串, 则不产生日志文件, 仅仅输出日志到屏幕.
 #define INIT_LOGGER(path, file) do {\
 	_LOCKS_();\
 	avhttp::aux::logger_single<avhttp::logger, boost::shared_ptr<avhttp::logger> >(path, file, true, true);\
 } while (0)
 
+///卸载日志模块接口.
 #define UNINIT_LOGGER() do {\
 	_LOCKS_();\
 	avhttp::aux::logger_single<avhttp::logger, boost::shared_ptr<avhttp::logger> >().reset();\
