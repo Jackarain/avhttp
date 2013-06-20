edca89941cc0bdfbe032a2699b28232a410f1df2
Jack <jack.wgm@gmail.com>
2013-06-20 17:32:25 +0800

    improve utf8.hpp
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- include/avhttp/detail/utf8.hpp
+++ include/avhttp/detail/utf8.hpp
@@ -23,29 +23,37 @@
 namespace avhttp {
 namespace detail {
 
-// 默认字符集编码.
-const static std::string default_characters = "GBK";
-
 // 字符集编码转换接口声明.
+
+// wide string to utf8 string.
 inline std::string wide_utf8(const std::wstring &source);
 
+// utf8 string to wide string.
 inline std::wstring utf8_wide(std::string const &source);
 
+// ansi string to utf8 string.
+inline std::string ansi_utf8(std::string const &source);
 inline std::string ansi_utf8(
-	std::string const &source, const std::string &characters = default_characters);
+	std::string const &source, const std::string &characters);
 
+// utf8 string to ansi string.
+inline std::string utf8_ansi(std::string const &source);
 inline std::string utf8_ansi(
-	std::string const &source, const std::string &characters = default_characters);
+	std::string const &source, const std::string &characters);
 
+// wide string to ansi string.
+inline std::string wide_ansi(const std::wstring &source);
 inline std::string wide_ansi(
-	std::wstring const &source, const std::string &characters = default_characters);
+	std::wstring const &source, const std::string &characters);
 
+// ansi string to wide string.
+inline std::wstring ansi_wide(const std::string &source);
 inline std::wstring ansi_wide(
-	const std::string &source, const std::string &characters = default_characters);
+	const std::string &source, const std::string &characters);
 
 // 字符集编码转换接口实现, 使用boost实现.
 inline std::wstring ansi_wide(
-	const std::string &source, const std::string &characters/* = default_characters*/)
+	const std::string &source, const std::string &characters)
 {
 	std::wstring destination;
 
@@ -55,14 +63,44 @@ inline std::wstring ansi_wide(
 	return destination;
 }
 
+inline std::wstring ansi_wide(const std::string &source)
+{
+	std::wstring wide;
+	wchar_t dest;
+	std::size_t max = source.size();
+
+	// reset mbtowc.
+	mbtowc(NULL, NULL, 0);
+
+	// convert source to wide.
+	for (std::string::const_iterator i = source.begin();
+		i != source.end(); )
+	{
+		int length = mbtowc(&dest, &(*i), max);
+		if (length < 1)
+			break;
+		max -= length;
+		while (length--) i++;
+		wide.push_back(dest);
+	}
+
+	return wide;
+}
+
 inline std::string ansi_utf8(
-	std::string const &source, const std::string &characters/* = default_characters*/)
+	std::string const &source, const std::string &characters)
 {
 	std::string destination;
 
 	destination = boost::locale::conv::between(source, "UTF-8", characters);
 
 	return destination;
+}
+
+inline std::string ansi_utf8(std::string const &source)
+{
+	std::wstring wide = ansi_wide(source);
+	return wide_utf8(wide);
 }
 
 inline std::string wide_utf8(const std::wstring &source)
@@ -93,8 +131,17 @@ inline std::string utf8_ansi(
 	return destination;
 }
 
+inline std::string utf8_ansi(std::string const &source)
+{
+	std::string destination;
+
+	destination = wide_ansi(utf8_wide(source));
+
+	return destination;
+}
+
 inline std::string wide_ansi(
-	std::wstring const &source, const std::string &characters/* = default_characters*/)
+	std::wstring const &source, const std::string &characters)
 {
 	std::string destination;
 
@@ -102,6 +149,29 @@ inline std::string wide_ansi(
 	destination = utf8_ansi(destination, characters);
 
 	return destination;
+}
+
+inline std::string wide_ansi(const std::wstring &source)
+{
+	std::size_t buffer_size = MB_CUR_MAX;
+	std::vector<char> buffer(buffer_size, 0);
+	std::string ansi;
+
+	// reset wctomb.
+	wctomb(NULL, NULL);
+
+	// convert source to wide.
+	for (std::wstring::const_iterator i = source.begin();
+		i != source.end(); i++)
+	{
+		int length = wctomb(buffer.data(), *i);
+		if (length < 1)
+			break;
+		for (int j = 0; j < length; j++)
+			ansi.push_back(buffer[j]);
+	}
+
+	return ansi;
 }
 
 } // namespace detail
