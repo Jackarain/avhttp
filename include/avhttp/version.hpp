1eb777f03f92e203c6cf1ad7af535e650fdcbe49
Jack <jack.wgm@gmail.com>
2013-09-07 11:49:59 +0800

    update version to 2.9.8
    Signed-off-by: Jack <jack.wgm@gmail.com>

--- include/avhttp/version.hpp
+++ include/avhttp/version.hpp
@@ -10,12 +10,12 @@
 
 #define AVHTTP_VERSION_MAJOR 2
 #define AVHTTP_VERSION_MINOR 9
-#define AVHTTP_VERSION_TINY 7
+#define AVHTTP_VERSION_TINY 8
 
 // the format of this version is: MMmmtt
 // M = Major version, m = minor version, t = tiny version
 #define AVHTTP_VERSION_NUM ((AVHTTP_VERSION_MAJOR * 10000) + (AVHTTP_VERSION_MINOR * 100) + AVHTTP_VERSION_TINY)
-#define AVHTTP_VERSION "2.9.7"
+#define AVHTTP_VERSION "2.9.8"
 #define AVHTTP_VERSION_MIME "avhttp/" AVHTTP_VERSION
 // #define AVHTTP_REVISION "$Git-Rev$"
 
