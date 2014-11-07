e2ac3eb36d3f3bc3419020338dfa498738db28fb
microcai <microcaicai@gmail.com>
2014-11-07 11:23:24 +0800

    boost 1.57 changed shared_pointer declaration scope
    fix issue #24

--- include/avhttp/multi_download.hpp
+++ include/avhttp/multi_download.hpp
@@ -23,6 +23,7 @@
 #include <boost/noncopyable.hpp>
 #include <boost/cstdint.hpp>
 #include <boost/shared_ptr.hpp>
+#include <boost/scoped_ptr.hpp>
 #include <boost/make_shared.hpp>
 #include <boost/thread.hpp>
 #include <boost/thread/condition.hpp>
